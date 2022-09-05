39cfe0eb772137e8dcb7afdbe9241e0b204207e0
“sergio <sergiowork98@gmail.com>
2022-09-05 14:12:21 -0300

    cambios de pagina, estilos, colores

---
+++ typed.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".autoType", {
+    strings: ["¡TE IMPULSAMOS <br> A DEJAR HUELLA!", "¡CON UN SOLO <br>CLICK!", "¿EMPEZAMOS?"],
+    typeSpeed: 60,
+    backSpeed: 50,
+    loop: true
+})
